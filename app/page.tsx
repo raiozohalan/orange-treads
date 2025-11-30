6d8ed408edd9cd19c6fed4ee03e9430c9c524b90
Joelhy Galan <raizohgalan@gmail.com>
2025-11-30 21:11:37 +0800

    Enhance Home component styling by adding a black background and padding for improved layout and visual appeal.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 h-screen">
+    <div className="flex flex-col items-center justify-center gap-4 h-screen bg-black px-10 py-5">
       <div className="flex flex-col items-center justify-center gap-4">
 
         <Image src="/logo.png" alt="Orange Treads" width={254} height={254} />
