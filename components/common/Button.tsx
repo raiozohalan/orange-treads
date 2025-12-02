7580e9f5b4c13c11596c4cd60deca7ae007258f8
Joelhy Galan <raizohgalan@gmail.com>
2025-12-02 22:52:51 +0800

    Add common UI components: Button, TextField, and LoadingSpinner; include SVG type definition

---
+++ components/common/Button.tsx
@@ -0,0 +1,57 @@
+import classNames from "@/utils/classNames";
+import React from "react";
+
+const buttonVariants = {
+  primary: "bg-blue-500 hover:bg-blue-600 text-white",
+  secondary: "bg-gray-500 hover:bg-gray-600 text-white",
+};
+
+const buttonSizes = {
+  small: "px-2 py-1 text-sm",
+  medium: "px-4 py-2 text-base",
+  large: "px-6 py-3 text-lg",
+};
+
+const roundedSizes = {
+  small: "rounded-sm",
+  medium: "rounded-md",
+  large: "rounded-lg",
+};
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: React.ReactNode;
+  variant?: keyof typeof buttonVariants;
+  size: keyof typeof buttonSizes;
+  roundedSize: keyof typeof roundedSizes;
+  fullWidth?: boolean;
+  className?: string;
+}
+
+const Button = ({
+  children,
+  variant = "primary",
+  size = "medium",
+  roundedSize = "medium",
+  fullWidth = false,
+  className = "",
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      {...props}
+      className={classNames(
+        "font-bold uppercase disabled:opacity-50 disabled:cursor-not-allowed disabled:pointer-events-none disabled:select-none",
+        "cursor-pointer transition-opacity duration-300 ease-in-out",
+        fullWidth ? "w-full" : "",
+        buttonVariants[variant],
+        buttonSizes[size],
+        roundedSizes[roundedSize],
+        className
+      )}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ components/common/TextField.tsx
@@ -0,0 +1,24 @@
+import classNames from "@/utils/classNames";
+import React from "react";
+
+interface TextFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  error?: string;
+}
+
+const TextField = ({ label, ...props }: TextFieldProps) => {
+  return (
+    <div className="flex flex-col gap-1 w-full">
+      {label && <label className="text-sm text-gray-400">{label}</label>}
+      <input
+        {...props}
+        className={classNames(
+          "w-full px-3 py-2 rounded-md border-2 border-gray-300 focus:outline-none focus:border-blue-500 transition-all duration-300 ease-in-out",
+          "disabled:opacity-50 disabled:cursor-not-allowed disabled:pointer-events-none disabled:select-none"
+        )}
+      />
+    </div>
+  );
+};
+
+export default TextField;
