4a837a172c5b2d9a266ffd025ea9e8b77adc43ce
Joelhy Galan <raizohgalan@gmail.com>
2025-12-11 21:39:52 +0800

    Update .gitignore to exclude function-related directories and files; modify Next.js configuration for standalone output and enable React strict mode; add new scripts to package.json for post-build tasks and Firebase serving.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "standalone",
   images: {
     unoptimized: true,
   },
-  trailingSlash: false,
+  reactStrictMode: true,
 }
 
 export default nextConfig
