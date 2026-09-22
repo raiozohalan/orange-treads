08b456cf9b5a5eed7ee008dbaf8a4785fe6f0e71
Joelhy Galan <raizohgalan@gmail.com>
2026-09-23 00:44:41 +0800

    feat: enhance Spin Wheel page with improved layout, group selection, and add buttons for group and price management

--- app/admin/(auth)/spin-wheel/page.tsx
+++ app/admin/(auth)/spin-wheel/page.tsx
@@ -9,6 +9,7 @@ import { useEffect } from "react"
 import { useAppStore } from "@/providers/app-store-provider"
 import Select from "@/components/common/Select"
 import SpinWheel from "@/components/common/SpinWheel"
+import { LifeBuoy, Plus } from "react-feather"
 
 const auth = getClientAuth()
 
@@ -26,26 +27,28 @@ const page = () => {
     const unsubscribe = async () => {
       const group = await getWheelGroupWithPrices()
       setGroup(group)
+      setCurrentGroup(group[0] || null)
     }
 
     unsubscribe()
   }, [])
 
   return (
-    <div className="w-full min-h-screen py-2">
-      <div className="w-1/2 px-6 py-4">
-        <SpinWheelGroupForm />
-        <SpinWheelPriceForm />
-        <div className="flex items-end justify-between gap-10 w-full">
+    <div className="px-6 py-8">
+      <div className="flex items-center text-2xl font-bold mb-4">
+        <h1 className="flex-1 flex items-center gap-2 leading-none text-gray-400">
+          <LifeBuoy size={24} /> Spin Wheel Admin
+        </h1>
+        <div className="flex-none flex items-end justify-between gap-2">
           <Select
-            label="Select Group"
+            label="Group"
             value={currentGroup?.id || ""}
             onChange={(e) => {
               const selectedGroup = groups.find((g) => g.id === e.target.value)
               setCurrentGroup(selectedGroup || null)
             }}
-            containerClassName="flex-none w-1/3"
-            className="border-px! py-1! h-10"
+            containerClassName="flex-none flex-row! items-center! gap-2! w-auto"
+            className="border-px! py-0! h-7! text-sm! max-w-40 overflow-hidden border-none! bg-gray-700/60! pr-6!"
           >
             <option value="">Select a group</option>
             {groups.map((group) => (
@@ -54,66 +57,77 @@ const page = () => {
               </option>
             ))}
           </Select>
-          <div className="flex-1 flex items-end justify-between gap-5">
-            <Button popoverTarget="spin-wheel-group-form" fullWidth>
-              Add Group
-            </Button>
-            <Button popoverTarget="spin-wheel-price-form" fullWidth>
-              Add Price
-            </Button>
-          </div>
+          <Button
+            variant="secondary"
+            size="small"
+            popoverTarget="spin-wheel-group-form"
+            className="text-sm gap-1! pr-2.5 capitalize! text-gray-300!"
+          >
+            <Plus className="w-4 h-4" /> Add Group
+          </Button>
+          <Button
+            variant="secondary"
+            size="small"
+            popoverTarget="spin-wheel-price-form"
+            className="text-sm gap-1! pr-2.5 capitalize! text-gray-300!"
+          >
+            <Plus className="w-4 h-4" /> Add Price
+          </Button>
         </div>
-        <div className="flex items-start justify-between gap-10 w-full mt-5">
-          <div className="flex-1 flex flex-col items-start justify-start gap-4">
-            <div className="w-full">
-              <h2 className="text-xl font-bold">{currentGroup?.name}</h2>
-              <p className="w-full text-sm text-pretty bg-slate-900 text-slate-300 overflow-clip px-3 py-1.5 rounded-md">
-                {currentGroup?.description}
-              </p>
-            </div>
-            <div className="flex-none flex flex-col gap-1 w-full">
-              {currentGroup?.prices.map((price) => (
-                <div
-                  key={price.id}
-                  className="flex items-start justify-between gap-3 bg-gray-900 px-3 py-2 rounded-md"
-                >
-                  <div className="w-10 h-auto">
-                    {price.image && (
-                      <img src={price.image} className="w-full h-auto" />
-                    )}
-                  </div>
-                  <div className="flex-1 flex flex-col items-start justify-stretch gap-1">
-                    <label className="flex-none flex items-center gap-1 font-bold">
-                      <div
-                        className="inline-block h-4 w-4 rounded-md"
-                        style={{
-                          backgroundColor: price.color,
-                        }}
-                      />
-                      {price.name}
-                    </label>
-                    <div className="relative flex-1 flex items-center w-full bg-gray-700 rounded-full overflow-hidden">
-                      <div
-                        className="absolute top-0 left-0 h-full rounded-full bg-green-500"
-                        style={{
-                          width: `${[price.percentage]}%`,
-                        }}
-                      />
-                      <span className="relative px-2 overflow-hidden text-xs text-black">
-                        {price.percentage}% Chance
-                      </span>
-                    </div>
+      </div>
+      <hr className="border-gray-800 mb-4" />
+      <SpinWheelGroupForm />
+      <SpinWheelPriceForm />
+      <div className="flex items-start justify-between gap-10 w-full mt-5">
+        <div className="flex-1 flex flex-col items-start justify-start gap-4">
+          <div className="w-full">
+            <h2 className="text-xl font-bold">{currentGroup?.name}</h2>
+            <p className="w-full text-sm text-pretty bg-slate-900 text-slate-300 overflow-clip px-3 py-1.5 rounded-md">
+              {currentGroup?.description}
+            </p>
+          </div>
+          <div className="flex-none flex flex-col gap-1 w-full">
+            {currentGroup?.prices.map((price) => (
+              <div
+                key={price.id}
+                className="flex items-start justify-between gap-3 bg-gray-900 px-3 py-2 rounded-md"
+              >
+                <div className="w-10 h-auto">
+                  {price.image && (
+                    <img src={price.image} className="w-full h-auto" />
+                  )}
+                </div>
+                <div className="flex-1 flex flex-col items-start justify-stretch gap-1">
+                  <label className="flex-none flex items-center gap-1 font-bold">
+                    <div
+                      className="inline-block h-4 w-4 rounded-md"
+                      style={{
+                        backgroundColor: price.color,
+                      }}
+                    />
+                    {price.name}
+                  </label>
+                  <div className="relative flex-1 flex items-center w-full bg-gray-700 rounded-full overflow-hidden">
+                    <div
+                      className="absolute top-0 left-0 h-full rounded-full bg-green-500"
+                      style={{
+                        width: `${[price.percentage]}%`,
+                      }}
+                    />
+                    <span className="relative px-2 overflow-hidden text-xs text-black">
+                      {price.percentage}% Chance
+                    </span>
                   </div>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
-          <SpinWheel
-            prizes={currentGroup?.prices || []}
-            onSpinEnd={(prize) => console.log("Winner:", prize)}
-            className="flex-none"
-          />
         </div>
+        <SpinWheel
+          prizes={currentGroup?.prices || []}
+          onSpinEnd={(prize) => console.log("Winner:", prize)}
+          className="flex-none"
+        />
       </div>
     </div>
   )
